9b18b1cc5f50cc2863b9c014d95bd67f3dccb4db
Miguel Garcia <miguelgarse@gmail.com>
2021-03-31 20:28:08 +0200

    DTO para Pedidos para mostrar informacion customizada en la tabla de Pedidos

---
+++ MaquetaTienda/Models/PedidoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaquetaTienda.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PedidoDto
+    {
+        public int Id { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public string Cliente { get; set; }
+    }
+}
